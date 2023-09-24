f4f4363c2fdebf982f55c786fbfce967258bc318
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-09-24 14:07:23 +0330

    edit

--- variables.js
+++ variables.js
@@ -1,10 +1,18 @@
 // doesn't work, don't know why!!!!
 // document.getElementById("headerColor")?.textContent = Telegram.WebApp.headerColor;
 
+// // headerColor
+// const headerColorElement = document.getElementById("headerColor");
+// headerColorElement.textContent = Telegram.WebApp.headerColor;
+
+// // version
+// const versionElement = document.getElementById("version");
+// versionElement.textContent = Telegram.WebApp.version;
+
 // headerColor
-const headerColorElement = document.getElementById("headerColor");
-headerColorElement.textContent = Telegram.WebApp.headerColor;
+var myElement = document.getElementById("headerColor");
+myElement.textContent = Telegram.WebApp.headerColor;
 
 // version
-const versionElement = document.getElementById("version");
-versionElement.textContent = Telegram.WebApp.version;
+var myElement = document.getElementById("version");
+myElement.textContent = Telegram.WebApp.version;
