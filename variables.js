f80e47f6f4912604a1c5d5d20f866e368a9f0bac
Arash Nemat Zadeh <arashnm80@gmail.com>
2023-09-24 13:54:13 +0330

    e

---
+++ variables.js
@@ -0,0 +1,4 @@
+// headerColor
+// const outputElement = document.getElementById("headerColor");
+// outputElement.textContent = Telegram.WebApp.headerColor;
+document.getElementById("headerColor")?.textContent = Telegram.WebApp.headerColor;
